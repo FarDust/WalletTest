7435eb55f3330c12d0c5262f86ffb8a5274370ac
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-09-13 18:21:07 -0300

    build: fix pointer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import "bootstrap"
-import "../assets/stylesheets/application"
+import "../../assets/stylesheets/application"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
