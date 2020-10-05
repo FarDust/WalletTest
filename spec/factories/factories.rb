28859dd320a451ffbf6825d238a8bbf651b8cb2a
Vicente Manriquez <vmanriquezcortes@gmail.com>
2020-10-04 21:11:41 -0300

    separated debt factory

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -6,51 +6,28 @@ FactoryBot.define do
     password { '123456' }
   end
 
-  factory :category do 
-    name {'comida'}
-    description {'categoria para los gastos de comida'}
+  factory :category do
+    name { 'comida' }
+    description { 'categoria para los gastos de comida' }
   end
 
-  factory :account do 
+  factory :account do
     user
-    balance {3000}
-    type {"credito"}
-    currency {"US"}
-    quota {"200"}
+    balance { 3000 }
+    type { 'credito' }
+    currency { 'US' }
+    quota { '200' }
   end
 
-  factory :natural_person do 
-    nombre {"Jhon"}
-    apelldio {"Doe"}
+  factory :natural_person do
+    nombre { 'Jhon' }
+    apelldio { 'Doe' }
   end
 
-  factory :debt do 
-    amount {3000}
-    for_user 
-
-    trait :for_user do
-      association :acreedor, factory: :user
-      association :deudor, factory: :user
-    end
-
-    trait :for_natural_acreedor do 
-      association :acreedor, factory: :natural_person
-      association :deudor, factory: :user
-    end
-
-    trait :for_natural_deudor do 
-      association :acreedor, factory: :user
-      association :deudor, factory: :natural_person
-    end
-    
-  end
-
-  factory :movement do 
+  factory :movement do
     category
     account
-    final_balance {2322}
-    amount {2323}
+    final_balance { 2322 }
+    amount { 2323 }
   end
-
-
 end
