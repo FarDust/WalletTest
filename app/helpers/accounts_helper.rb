bf67d23e14cd6ba65d3fa2b724fb84cf08253c1a
Marcelo Riveros <mgriveros@uc.cl>
2020-10-11 17:46:18 -0300

    feat(movements): added helper to render the form depending on account type

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module AccountsHelper
+  def account_form(account)
+    case account.account_type
+    when Account::DEBT_TYPE
+      "debt_form"
+    when Account::CREDIT_TYPE
+      "credit_form"
+    when Account::COMMON_TYPE
+      "common_form"
+    else
+      "form"
+    end
+  end
 end
