99e07e72eab9de4930bf3d82bf6689eae1713908
aecanales <aecanales@uc.cl>
2020-10-11 19:45:33 -0300

    feat: add non-positive credit balance validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -17,5 +17,12 @@ class Account < ApplicationRecord
   belongs_to :user
   validates :balance_cents, presence: true
   validates :account_type, presence: true
+  validate :credit_does_not_have_positive_balance
   monetize :balance_cents
+
+  def credit_does_not_have_positive_balance
+    if account_type == "credit" && balance > 0
+      errors.add(:positive_credit, 'A credit account cannot have a positve balance.')
+    end
+  end
 end
