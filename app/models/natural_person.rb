1080a3d0708ee4e13439049e2d971334e2d4726d
felipegarcia98 <fagarcia5@uc.cl>
2020-12-03 04:30:28 -0300

    fix(models): Actualization of models with new changes in schema

--- app/models/natural_person.rb
+++ app/models/natural_person.rb
@@ -7,14 +7,16 @@
 #  id         :bigint           not null, primary key
 #  nombre     :string
 #  apellido   :string
-#  rut        :string
+#  related_account    :bigint
+#########  rut        :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class NaturalPerson < ApplicationRecord
   validates :nombre, presence: true
   validates :apellido, presence: true
-  validates :rut, presence: true
+  # validates :rut, presence: true
+
   has_many :debts, as: :deudor, dependent: :restrict_with_error
   has_many :debts, as: :acreedor, dependent: :restrict_with_error
 end
